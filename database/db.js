4eff399251ed03498be1f014a59a07e7ac92c448
Hanewich <john-hanewich@idexx.com>
2019-05-21 12:19:53 -0400

    mychanges

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 //let connectionString = config.get("server.connectionstring");
-let connectionString = "mongodb://user:user123@ds155192.mlab.com:55192/idexxnode";
+let connectionString = "mongodb+srv://user123:user123@cluster0-u71cb.mongodb.net/test?retryWrites=true";
 console.log(connectionString);
 
 module.exports = function () {
